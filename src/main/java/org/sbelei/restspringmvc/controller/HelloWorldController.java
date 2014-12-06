fe36a59916ec829cd17a0a769524ed514bd7a7fa
Sergiy Beley <Sergiy.Beley@gmail.com>
2014-12-06 14:53:13 +0200

    add simple response

--- src/main/java/org/sbelei/restspringmvc/controller/HelloWorldController.java
+++ src/main/java/org/sbelei/restspringmvc/controller/HelloWorldController.java
@@ -4,16 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
 
 	@RequestMapping("/hello")
-	public String hello(
+	public @ResponseBody String hello(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name,
 			Model model) {
 		model.addAttribute("name", name);
-		return "helloworld";
+		return "responce";
 	}
 
 }
