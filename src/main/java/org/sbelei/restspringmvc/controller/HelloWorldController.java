cb55bac98616b6ac81a05b8b8d7ab180d7021306
Sergiy Beley <Sergiy.Beley@gmail.com>
2014-12-06 17:17:09 +0200

    add path parameter instead of request parameter. URL to use : http://localhost:8080/restspringmvc/hello/FooBar

--- src/main/java/org/sbelei/restspringmvc/controller/HelloWorldController.java
+++ src/main/java/org/sbelei/restspringmvc/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package org.sbelei.restspringmvc.controller;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 
-	@RequestMapping(value = "/hello", produces = {MediaType.APPLICATION_JSON_VALUE})
+	@RequestMapping(value = "/hello/{name}", produces = {MediaType.APPLICATION_JSON_VALUE})
 	public @ResponseBody Customer hello(
-			@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			@PathVariable(value = "name") String name,
 			Model model) {
 		Customer customer = new Customer();
 		customer.setName(name);
